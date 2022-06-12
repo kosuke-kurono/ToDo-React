7f1bbab3e58b830b2d03a934d23ca321e8635e17
kosuke-kurono <76575876+kosuke-kurono@users.noreply.github.com>
2022-06-12 20:35:48 +0900

    課題登録処理を作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,72 @@
 import React from "react";
+import { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [addText, setAddText] = useState("");
+  const [incompleteList, setIncompleteList] = useState(["掃除", "皿洗い"]);
+  const [completeList, setCompleteList] = useState(["犬の散歩", "買い物"]);
+  const onChangeText = (event) => setAddText(event.target.value);
+  const onClickAdd = () => {
+    if (addText !== "") {
+      const newText = [...incompleteList, addText];
+      setIncompleteList(newText);
+      setAddText("");
+    }
+  };
+  const onClickDelete = (index) => {
+    const newText = [...incompleteList];
+    newText.splice(index, 1);
+    setIncompleteList(newText);
+  };
+  const onClickComplete = (index) => {
+    const incompleteText = [...incompleteList];
+    incompleteText.splice(index, 1);
+
+    const completeText = [...completeList, incompleteList[index]];
+    setIncompleteList(incompleteText);
+    setCompleteList(completeText);
+  };
   return (
     <>
       <div className="container">
-        <div className="input-area border">
-          <input type="text" placeholder="入力してください" />
-          <button>登録</button>
+        <h1>ToDo</h1>
+        <div className="input-area">
+          <input
+            type="text"
+            placeholder="入力してください"
+            value={addText}
+            onChange={onChangeText}
+          />
+          <button onClick={onClickAdd}>登録</button>
         </div>
         <div className="incomplete box">
           <p>課題</p>
           <ul>
-            <div>
-              <li>あああ</li>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-            <div>
-              <li>いいい</li>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
+            {incompleteList.map((todo, index) => {
+              return (
+                <div key={todo} className="list">
+                  <li>{todo}</li>
+                  <div>
+                    <button onClick={() => onClickComplete(index)}>完了</button>
+                    <button onClick={() => onClickDelete(index)}>削除</button>
+                  </div>
+                </div>
+              );
+            })}
           </ul>
         </div>
         <div className="complete box">
           <p>完了</p>
           <ul>
-            <div>
-              <li>ううう</li>
-              <button>未完了に戻す</button>
-            </div>
+            {completeList.map((todo) => {
+              return (
+                <div key={todo} className="list">
+                  <li>{todo}</li>
+                  <button>課題に戻す</button>
+                </div>
+              );
+            })}
           </ul>
         </div>
       </div>
